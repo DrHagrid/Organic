157b7a55d613f3aed13cd24cc31ccd09f8947db4
DrHagrid <mail.hagrid@gmail.com>
2017-11-26 11:30:44 +0300

    Add fields in objects

--- reactions/models.py
+++ reactions/models.py
@@ -4,10 +4,12 @@ from classes.models import OrganicClass
 
 class Reaction(models.Model):
     organic_class = models.ForeignKey(OrganicClass)
-    name = models.CharField(max_length=64)
-    materials = models.CharField(max_length=128)
-    products = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    materials = models.CharField(max_length=256)
+    products = models.CharField(max_length=256)
     conditions = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    picture = models.ImageField(upload_to='reactions', blank=True, null=True)
 
     def __str__(self):
         return "%s %s" % (self.organic_class.name, self.name.lower())
@@ -19,13 +21,15 @@ class Reaction(models.Model):
 
 class ReceivingReaction(models.Model):
     organic_class = models.ForeignKey(OrganicClass)
-    name = models.CharField(max_length=64)
-    materials = models.CharField(max_length=128)
-    products = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    materials = models.CharField(max_length=256)
+    products = models.CharField(max_length=256)
     conditions = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    picture = models.ImageField(upload_to='reactions', blank=True, null=True)
 
     def __str__(self):
-        return "%s %s" % (self.organic_class.name, self.name.lower())
+        return "%s | %s" % (self.organic_class.name, self.name.lower())
 
     class Meta:
         verbose_name = 'Реакция получения'
